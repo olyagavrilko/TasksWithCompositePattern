a73b7c1302949cbcc4cb390f51f128cc0b8552d8
Olya Gavrilko <olyaganeva@gmail.com>
2022-03-15 18:31:07 +0300

    first commit

---
+++ TasksWithCompositePattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TasksWithCompositePattern
+//
+//  Created by Olya Ganeva on 15.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
